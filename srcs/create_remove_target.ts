1de0715648d167926eb4b8eb1b2397aa8f440301
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-07-31 14:26:44 +0900

    fix(create_remove_target.ts): rename file to create_remove_target.ts

--- srcs/create_remove_target.ts
+++ srcs/create_remove_target.ts
@@ -1,10 +1,23 @@
-function create_target_domain_list() {
+function manual_create_remove_target() {
 	let confirmation = Browser.msgBox('ドメインリスト抽出処理', '本当に実行しますか？', Browser.Buttons.OK_CANCEL);
 	if (confirmation == "cancel") {
 		return ;
 	}
-
 	const SHEET = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+	create_remove_target(SHEET);
+}
+
+function auto_create_remove_info_ftp() {
+	const SHEET = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('RemoveInfoFtp');
+	create_remove_target(SHEET);
+}
+
+function auto_create_remove_info_123() {
+	const SHEET = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('RemoveInfo123');
+	create_remove_target(SHEET);
+}
+
+function create_remove_target(SHEET) {
 	SHEET.clear();
 	if (SHEET.getFilter())
 		SHEET.getFilter().remove();
@@ -12,34 +25,39 @@ function create_target_domain_list() {
 	SHEET.getRange('B1').setValue('ドメイン名').setBackground('#c9daf8');
 	SHEET.getRange('C1').setValue('Size').setBackground('#c9daf8');
 	SHEET.getRange('D1').setValue('=ArrayFormula(match(0,len(B2:B),0))-1');
-	SHEET.getRange('A1:N1').setFontFamily('Meiryo')
+	SHEET.getRange('A1:D1').setFontFamily('Meiryo')
 		.setFontWeight('bold')
+	SHEET.getRange('A1:F1').setFontFamily('Meiryo')
 		.setHorizontalAlignment('center')
 		.setVerticalAlignment('middle');
-	for (let col = 1; col <= 4; col++) {
+	for (let col = 1; col <= 6; col++) {
 		if (col == 1)
 			SHEET.setColumnWidth(col, 110);
 		else if (col == 2)
 			SHEET.setColumnWidth(col, 200);
-		else
+		else if (col == 3 || col == 4)
 			SHEET.setColumnWidth(col, 70);
+		else
+			SHEET.setColumnWidth(col, 100);
 	}
 	SHEET.setRowHeight(1, 40);
 	SHEET.setFrozenRows(1);
 	let targetDomainList: Array<Array<string>>;
 	if (SHEET.getSheetName() == 'RemoveInfoFtp')
-		targetDomainList = get_target_domain_list('登録中ドメイン（FTPサーバー）')
+		targetDomainList = get_remove_target('登録中ドメイン（FTPサーバー）')
 	else if (SHEET.getSheetName() == 'RemoveInfo123')
-		targetDomainList = get_target_domain_list('登録中ドメイン（123サーバー）')
+		targetDomainList = get_remove_target('登録中ドメイン（123サーバー）')
 	if (targetDomainList.length < 1)
 		return ;
 	SHEET.getRange(2, 1, targetDomainList.length, 2)
 		.setValues(targetDomainList)
 		.setFontFamily('Meiryo');
+	SHEET.getRange('F1').setValue(Utilities.formatDate(new Date(), 'JST', 'yyyy-MM-dd'))
+		.setBackground('#efefef');
 	SHEET.getRange(1, 1, SHEET.getLastRow(), 2).createFilter();
 }
 
-function get_target_domain_list(sheetName: string) {
+function get_remove_target(sheetName: string) {
 	const SPREADSHEET_ID = PropertiesService.getScriptProperties().getProperty('UNDER_CONTRACT_SSID');
 	const TARGET_SHEET = SpreadsheetApp.openById(SPREADSHEET_ID).getSheetByName(sheetName);
 	
